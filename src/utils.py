b9803c384df3026ceb229c37c3bedffba243da42
xueyumei <xueyumei@veredholdings.com>
2023-05-06 10:06:19 +0800

    update package.json, utils.py and vs.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import requests
+from bs4 import BeautifulSoup
 
 def download_pdf(url, filename):
     response = requests.get(url)
     with open(filename, 'wb') as f:
         f.write(response.content)
     print("Successfully downloaded" + filename)
+
+# 从html抓取数据
+def get_soup(url):
+    # url = 'https://caibaoshuo.com/companies/{}/financials'.format(code)
+    rsp = requests.get(url)
+    rsp.raise_for_status()
+    html = rsp.text
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
